7888fc0fc5262e956fdfb73fa70afae2b2b67f2a
monika isave <163721108+monikaisave@users.noreply.github.com>
2026-06-03 15:39:56 +0530

    Create ex.c

---
+++ ex.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,sum;
+
+    printf("Enter two numbers")
+    scanf("%d %d",&a,b);
+
+    sum = a + b
+
+    if(sum = 10)
+    {
+        printf("Sum is 10\n");
+    }
+    else
+    {
+        printf("Sum is not 10\n")
+    }
+
+    for(i=0;i<5;i++)
+    {
+        printf("%d\n",i)
+    }
+
+    return
+}
