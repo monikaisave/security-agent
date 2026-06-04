8f0091127e91a57afa22d694f718bd32be45dd4b
samarth <samarth@LAPTOP-KGU6CGC0>
2026-06-04 11:52:16 +0530

    Security Fix: Security Vulnerability Found in C:\Users\samarth\AppData\Local\Temp\tmp6tmlmjs9\ex.c

--- ex.c
+++ ex.c
@@ -4,24 +4,33 @@ int main()
 {
     int a,b,sum;
 
-    printf("Enter two numbers")
-    scanf("%d %d",&a,b);
+    printf("Enter two numbers");
+    // Use fgets() instead of scanf() to prevent buffer overflows
+    if(fgets(buffer, sizeof(buffer) - 1, stdin))
+    {
+        sscanf(buffer, "%d %d", &a, &b);
+    }
+    else
+    {
+        printf("Invalid input\n");
+        return;
+    }
 
-    sum = a + b
+    sum = a + b;
 
-    if(sum = 10)
+    if(sum == 10)
     {
         printf("Sum is 10\n");
     }
     else
     {
-        printf("Sum is not 10\n")
+        printf("Sum is not 10\n");
     }
 
     for(i=0;i<5;i++)
     {
-        printf("%d\n",i)
+        printf("%d\n",i);
     }
 
-    return
+    return;
 }
